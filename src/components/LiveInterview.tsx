7d9b6dd2baa8309415937b6f1b41287f170eb8b1
Venkata Sai Tanuja <tanujamannem@gmail.com>
2026-09-16 14:52:16 +0530

    Update LiveInterview.tsx

--- src/components/LiveInterview.tsx
+++ src/components/LiveInterview.tsx
@@ -112,8 +112,7 @@ interface InterviewReportData {
    API
    ========================================================= */
 
-const API_BASE_URL =
-  "http://127.0.0.1:8000";
+const API_BASE_URL = "https://intellihire-backend-pyb0.onrender.com";
 
 /* =========================================================
    SESSION STORAGE KEY
